4751101c4dc61bb31efbc9f81217a427c8080b38
Therafloop <juanfegenio@hotmail.com>
2017-05-15 10:24:27 -0500

    Nested Arrays 2

---
+++ Prueba25.rb
@@ -0,0 +1,14 @@
+i = 0
+while i < 3
+  p gato = Array.new(3) {["x","o"].sample}
+  print "\n"
+   i += 1
+end
+puts
+3.times{p gato = Array.new(3) {["x","o"].sample}; print "\n"}
+	
+  p  Hash["Nombre","Rodrigo García","Edad",13,"Genero","Masculino","Grupo","Primero","Calificaciones",[9, 9, 7, 6, 8]]
+  p  Hash["Nombre","Fernanda Gonzales","Edad",12,"Genero","Femenino","Grupo","Tercero","Calificaciones",[6, 9, 8, 6, 8]]
+  p  Hash["Nombre","Luis Perez","Edad",13,"Genero","Masculino","Grupo","Primero","Calificaciones",[8, 7, 7, 9, 8]]
+  p  Hash["Nombre","Ana Esponsa","Edad",14,"Genero","Femenino","Grupo","Segundo","Calificaciones",[9, 9, 6, 8, 8]]
+  p  Hash["Nombre","Pablo Moran","Edad",11,"Genero","Masculino","Grupo","Segundo","Calificaciones",[7, 8, 9, 9, 8]]
